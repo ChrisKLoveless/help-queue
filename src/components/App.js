76cd71ebaa0e153ab17fe93fb80c4c9e70d06540
ChrisKLoveless <chriskloveless@gmail.com>
2023-02-06 19:22:48 -0800

    update with button, event handler, and ticket components

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,14 @@
 import React from "react";
 import Header from "./Header";
-import Ticket from "./Ticket.js";
-import TicketList from "./TicketList";
+import TicketControl from "./TicketControl";
 import "../index.css";
 
 
 function App() {
-  const name = "Thato";
-  const name2 = "Haley";
   return (
     <React.Fragment>
       <Header />
-      {/* <h1>Help Queue</h1>    THIS CODE IS REPLACED BY HEADER COMPONENT */}
-      <TicketList />
-      {/* <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>    THIS COSE REPLACED BY TICKETLIST COMPONENT */}
+      <TicketControl />
       <hr />
     </React.Fragment>
   );
