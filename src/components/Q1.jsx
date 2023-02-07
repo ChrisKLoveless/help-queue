68a9d9ae1d484e1ef0ebd41310abbd3eda71a627
ChrisKLoveless <chriskloveless@gmail.com>
2023-02-07 09:29:51 -0800

    add q1 component
    Co-Authored-By: David Gamble <daviddgamble@gmail.com>

---
+++ src/components/Q1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Q1() {
+    <React.Fragment>
+        <p id="q1">"Have you gone through all the steps on the Learn How to Program debugging lesson?"</p>
+    </React.Fragment>
+}
+
+export default Q1;
--- src/components/TicketControl.jsx
+++ src/components/TicketControl.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
+import Q1 from './Q1';
 
 class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      q1VisibleOnPage: false,
+      q2VisibleOnPage: false,
+      q3VisibleOnPage: false,
     };
-    this.handleClick = this.handleClick.bind(this);
   }
-
-  handleClick = () => {
+  
+  formHandleClick = () => {
     this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
+      formVisibleOnPage: !prevState.formVisibleOnPage,
+      q1VisibleOnPage: !prevState.q1VisibleOnPage,
+      q2VisibleOnPage: !prevState.q2VisibleOnPage,
+      q3VisibleOnPage: !prevState.q3VisibleOnPage,
     }));
   }
 
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.formVisibleOnPage) {
+    if (this.state.q1VisibleOnPage) {
       currentlyVisibleState = <NewTicketForm />
       buttonText = "Return to Ticket List";
-    } 
+    } else if(this.state.q2VisibleOnPage) {
+
+    }
     else {
       currentlyVisibleState = <TicketList />
       buttonText = "Add Ticket";
@@ -32,7 +40,7 @@ class TicketControl extends React.Component {
     return (
       <React.Fragment>
         {currentlyVisibleState}
-      <button onClick={this.handleClick}> {buttonText} </button>
+      <button onClick={this.formHandleClick}> {buttonText} </button>
       </React.Fragment>
     );
   }
