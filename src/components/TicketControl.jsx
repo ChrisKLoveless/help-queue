adec49374931d051aea1cfa19f1d2e355c1ca79c
ChrisKLoveless <chriskloveless@gmail.com>
2023-02-08 08:22:59 -0800

    fix bug

--- src/components/TicketControl.jsx
+++ src/components/TicketControl.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
-import Q1 from './Q1';
 
 class TicketControl extends React.Component {
 
@@ -9,9 +8,6 @@ class TicketControl extends React.Component {
     super(props);
     this.state = {
       formVisibleOnPage: false,
-      q1VisibleOnPage: false,
-      q2VisibleOnPage: false,
-      q3VisibleOnPage: false,
       mainTicketList: []
     };
   }
@@ -25,21 +21,16 @@ class TicketControl extends React.Component {
   formHandleClick = () => {
     this.setState(prevState => ({
       formVisibleOnPage: !prevState.formVisibleOnPage,
-      q1VisibleOnPage: !prevState.q1VisibleOnPage,
-      q2VisibleOnPage: !prevState.q2VisibleOnPage,
-      q3VisibleOnPage: !prevState.q3VisibleOnPage,
     }));
   }
 
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.q1VisibleOnPage) {
+    if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
       buttonText = "Return to Ticket List";
-    } else if(this.state.q2VisibleOnPage) {
-
-    }
+    } 
     else {
       currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} />
       buttonText = "Add Ticket";
