ec003216cd287f39a5ad0fccee680ea2c8bb9f43
ChrisKLoveless <chriskloveless@gmail.com>
2023-02-14 14:31:41 -0800

    use legacy-createStore import

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import { legacy_createStore as createStore} from 'redux'
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
 
-
 const store = createStore(rootReducer);
+
 store.subscribe(() =>
 console.log(store.getState()) 
 );
