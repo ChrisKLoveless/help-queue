1567f6a4061c6e3584a07e0d8d19677a4cae3ed2
ChrisKLoveless <chriskloveless@gmail.com>
2023-02-14 16:37:11 -0800

    add action creators

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -4,6 +4,7 @@ import formVisibleReducer from '../../reducers/form-visible-reducer';
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 
 let store = createStore(rootReducer);
+
 describe("rootReducer", () => {
 
   test('Should return default state if no action type is recognized', () => {
